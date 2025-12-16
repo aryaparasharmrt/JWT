222fc4e6cfff76f8a0d10fdc2b64ce1317fbc3d7
aryaparasharmrt <aryaparasharj02@gmail.com>
2025-12-16 15:48:43 +0530

    Update in person entity

--- src/main/java/com/testprojg/testproja/Entity/Person.java
+++ src/main/java/com/testprojg/testproja/Entity/Person.java
@@ -1,5 +1,6 @@
 package com.testprojg.testproja.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
@@ -8,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Entity
 @Getter
 @Setter
@@ -24,6 +26,14 @@ public class Person implements UserDetails {
         this.name = name;
     }
 
+    // 👇 ADD THIS FIELD
+    @Transient // <-- IMPORTANT: do NOT save to DB
+    private List<GrantedAuthority> authorities = List.of();
+
+    // 👇 ADD THIS SETTER so Jackson can deserialize
+    public void setAuthorities(List<GrantedAuthority> authorities) {
+        this.authorities = authorities == null ? List.of() : authorities;
+    }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
